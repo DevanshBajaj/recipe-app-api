c58dd2ed939ab22be7c9ea562df547023be062cd
Devansh Bajaj <devansh21db.db@gmail.com>
2020-07-02 21:56:12 +0530

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*vjk6l2)u(3&=&(g_wp!!eabbg4^@=56&f$r1tgdw@k@jg+c!&'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
